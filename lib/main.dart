01468c5b284f392a6c0fe95ce09c1e01060fe16d
NadaKattan <nadakattan012@gmail.com>
2024-11-07 02:06:52 +0200

    init project and add splash screen

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bookly/Features/splash/presentation/views/splash_view.dart';
+import 'package:bookly/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+      home: SplashView(),
+    );
+  }
+}
